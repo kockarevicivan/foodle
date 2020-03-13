bfae230b18c04889fdc2a292c92e48317f5a4785
Rade Knezeivc <radeknezevic81@gmail.com>
2020-03-13 11:57:00 +0100

    Change userService

--- api/src/Services/UserService.ts
+++ api/src/Services/UserService.ts
@@ -1,26 +1,35 @@
 import User from "../models/User";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 class UserService {
-    async add(user: any) {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        user.password = hashedPassword;
-
-        return await User.create(user);
+    async add(userPayload: any) {
+        const hashedPassword = await bcrypt.hash(userPayload.password, 10);
+        userPayload.password = hashedPassword;
+        const user = await User.create(userPayload);
+        const id = user._id;
+        const nest = User.findById(id).select(["username", "fullName"]);
+        return nest;
     }
 
     async getAll() {
-        return await User.find({});
+        return await User.find({}).select(["username", "fullname"]);
     }
 
     async getById(id: string) {
-        return await User.findById(id);
+        return await User.findById(id).select(["username", "fullname"]);
     }
 
-    async update(id: string, updateOps: Object) {
-        return await User.findByIdAndUpdate(id, { $set: updateOps });
+    async update(id: string, update: Object) {
+        return await User.findByIdAndUpdate(id, { $set: update }).select([
+            "username",
+            "fullname"
+        ]);
     }
+
     async delete(id: string) {
-        return await User.findByIdAndDelete(id);
+        return await User.findByIdAndDelete(id).select([
+            "username",
+            "fullname"
+        ]);
     }
 }
 
