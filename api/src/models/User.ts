60e8716aaf4c06c888d2d2fae1433ad4c1aa0e6b
Mijat <mijat.mlietic98@gmail.com>
2020-03-13 10:57:10 +0100

    Update user model

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
 export default mongoose.model(
-    "users",
-    new mongoose.Schema({
-        username: { type: String, unique: true },
-        fullName: String,
-        password: String,
-        salt: String,
-        refreshToken: String
-    })
+  "users",
+  new mongoose.Schema({
+    username: { type: String, unique: true },
+    fullName: String,
+    password: String,
+    refreshToken: String,
+    role: String,
+    weeklyReceipt: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "weeklyReceipt"
+      }
+    ]
+  })
 );
