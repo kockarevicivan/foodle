4c077ca41a4db4b73ee61b54781eec54ff9fdad0
Rade Knezevic <radeknezevic81@gmail.com>
2020-03-27 13:11:12 +0100

    Create table component

--- client/src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import Layout from "../../../components/Layout/Layout";
-
+import MenuTable from "../../../components/MenuTableComponent/MenuTableComponent"
 class AdminDashboard extends Component {
   state = {};
   render() {
     return (
       <Layout>
-        <h1>AdminDashboard</h1>;
+        <MenuTable></MenuTable>
       </Layout>
     );
   }
