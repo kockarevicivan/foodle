ac6ca76f1dad72e3c54fb5f59b26296b8cff000c
Mijat019 <mijat.miletic98@gmail.com>
2020-03-28 18:49:20 +0100

    Create manage users components

---
+++ client/src/pages/ManageUsers/ManageUser.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class ManageUsers extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>ManageUsers</h1>
+        <p>this is a page</p>
+      </div>
+    );
+  }
+}
+
+export default ManageUsers;
