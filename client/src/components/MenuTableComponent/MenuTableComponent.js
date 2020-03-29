d6adc17344b33d8adebee42ee8b423e8c274744b
Rade Knezevic <radeknezevic81@gmail.com>
2020-03-29 12:19:22 +0200

    Rework table component

--- client/src/components/MenuTableComponent/MenuTableComponent.js
+++ client/src/components/MenuTableComponent/MenuTableComponent.js
@@ -1,58 +1,55 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, { Component } from 'react';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Pire krompir', 35,'100gr'),
-  createData('Kolac sa kokosom', 65,'kom'),
-  createData('Pilece belo meso', 75, '100gr'),
-  createData('Cupcake', 45,'kom'),
-  createData('Gingerbread', 356,'kom'),
-];
-
-export default function MenuTable() {
-  const classes = useStyles();
-
-  return (
-    <div className="col-6">
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Naziv</TableCell>
-            <TableCell align="right">Cena</TableCell>
-            <TableCell align="right">100gr/1kom</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+class MenuTable extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Naziv</th>
+                  <th scope="col">Cena</th>
+                  <th scope="col">100gr/1kom</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Pohovana piletina</td>
+                  <td>75</td>
+                  <td>100gr</td>
+                  <td>
+                    <button>+</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Pire krompir</td>
+                  <td>35</td>
+                  <td>100gr</td>
+                  <td>
+                    <button>+</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td >Kolac sa kokosom</td>
+                  <td>60</td>
+                  <td>1kom</td>
+                  <td>
+                    <button>+</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-  );
+        </div>
+      </div>
+    );
+  }
 }
+ 
+export default MenuTable;
+
+
