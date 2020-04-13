ceb3d5c5ee0194a8bfdffafbefcc6aaf777c60b0
Mijat019 <mijat.miletic98@gmail.com>
2020-04-13 20:19:00 +0200

    Implement generate pdf

--- client/src/components/Tabs/OrderOverview/OrderOverview.jsx
+++ client/src/components/Tabs/OrderOverview/OrderOverview.jsx
@@ -19,6 +19,8 @@ import {
 import { compose } from "redux";
 import { withStyles } from "@material-ui/styles";
 import EnterPriceButton from "./EnterPriceButton";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 class OrderOverview extends Component {
   state = {};
@@ -30,12 +32,34 @@ class OrderOverview extends Component {
   sendOrders = async () => {
     const { orders, sendAllOrders, menu } = this.props;
     const orderIds = orders?.map((order) => order._id);
+    this.generatePdf();
     try {
       await sendAllOrders(orderIds, menu?._id);
       alert("All orders have been sent.");
     } catch (error) {
       alert(error);
     }
+  };
+
+  generatePdf = () => {
+    const data = this.props.orders.map((order) => {
+      let item = [order.user.fullName];
+      const hrana = order.orderItems
+        .map((item) => `${item.title} ${item.quantity}`)
+        .join(`\n`);
+      item.push(hrana);
+      return item;
+    });
+
+    let doc = new jsPDF({ putOnlyUsedFonts: true, orientation: "landscape" });
+    const date = `${new Date().getDate()}.${new Date().getMonth() + 1}`;
+    doc.text(date, 1, 5);
+    doc.autoTable({
+      theme: "grid",
+      head: [["ime", "hrana"]],
+      body: data,
+    });
+    doc.save(`${date}.pdf`);
   };
 
   render() {
